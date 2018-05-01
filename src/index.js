2ca23ec3661938b764301d76c571df1de6cf9816
Feigh <fey.vegborn@gmail.com>
2018-05-01 12:10:29 +0200

    Uppdaterat med lint stöd

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world bru!');
+ReactDOM.render(element, document.getElementById('root'));
+registerServiceWorker();;
